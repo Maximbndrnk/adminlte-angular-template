a48214e072f765ad10b4d987be8a57fd1b1f35f3
Maxim Bondarenko <maximbndrnk@gmail.com>
2025-01-11 09:47:21 +0200

    add base routes and test table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
   {
-    path: 'user',
+    path: 'users',
     loadComponent: () =>
       import('./user/create-user/create-user.component').then((m) => m.CreateUserComponent),
+  },
+  {
+    path: 'admins',
+    loadComponent: () =>
+      import('./user/create-user/create-user.component').then((m) => m.CreateUserComponent),
+  },
+  {
+    path: 'localizations',
+    loadComponent: () =>
+      import('./translations/translations.component').then((m) => m.TranslationsComponent),
+  },
+  {
+    path: 'log',
+    loadComponent: () =>
+      import('./translations/translations.component').then((m) => m.TranslationsComponent),
   }
 ];
